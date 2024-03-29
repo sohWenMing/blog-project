461b1fe90aed915cbdf0261f8e46b89a6973e153
Wen Ming Soh <wenming.soh@gmail.com>
2024-03-30 07:24:14 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    'plugins': [
+        '@stylistic/js'
+    ],
+    'env': {
+        'node': true,
+        'commonjs': true,
+        'es2021': true
+    },
+    'extends': 'eslint:recommended',
+    'overrides': [
+        {
+            'env': {
+                'node': true
+            },
+            'files': [
+                '.eslintrc.{js,cjs}'
+            ],
+            'parserOptions': {
+                'sourceType': 'script'
+            }
+        }
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest'
+    },
+    'rules': {
+        '@stylistic/js/indent': ['error', 4],
+        'no-console': 'off',
+        '@stylistic/js/quotes': ['error', 'single'],
+        '@stylistic/js/space-in-parens': ['error', 'always'],
+        '@stylistic/js/linebreak-style': ['error', 'unix'],
+        '@stylistic/js/semi': ['error', 'always'],
+        '@stylistic/js/no-trailing-spaces': 'error'
+    }
+};
