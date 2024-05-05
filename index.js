b68956d131e04fca5d6f30728ebe1ab7f8757d8c
Wen Ming Soh <wenming.soh@gmail.com>
2024-05-05 08:43:03 +0800

    working on env

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const { app } = require('./app');
 const { info } = require('./utils/logger');
 
+const ENV = process.env.NODE_ENV;
+info('NODE_ENV: ', ENV);
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
