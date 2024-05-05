baae5a67023118fc09e0ff8e2a0185561380ed7b
Wen Ming Soh <wenming.soh@gmail.com>
2024-05-05 08:54:12 +0800

    fixing env

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const { errorHandler } = require('./utils/errorUtils');
 const { generateAndThrowError } = require('./utils/errorUtils/errorGenerator');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const origin = process.env.NODE_ENV !== 'PROD' ? 'http://localhost:5173' : 'https://blog-front-end-p8zv.onrender.com/';
+const origin = process.env.NODE_ENV !== 'PROD' ? 'http://localhost:5173' : 'https://blog-front-end-p8zv.onrender.com';
 
 app.use(cors({
     // origin: origin,
